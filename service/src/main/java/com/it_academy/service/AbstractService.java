c2e85a06bbe6943c18f61079217e744c057f8545
PavelDrozd <87943574+PavelDrozd@users.noreply.github.com>
2023-03-22 14:20:58 +0300

    Create UserDto class with default fields, create AbstractService and UserService interfaces

---
+++ service/src/main/java/com/it_academy/service/AbstractService.java
@@ -0,0 +1,18 @@
+package com.it_academy.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface AbstractService<T, K> {
+
+    T create(T t);
+
+    Page<T> getAll(Pageable pageable);
+
+    T get(K id);
+
+    T update(T t);
+
+    void delete(K id);
+
+}
---
+++ service/src/main/java/com/it_academy/service/dto/UserDto.java
@@ -0,0 +1,21 @@
+package com.it_academy.service.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String patronymic;
+
+    private String email;
+
+    public enum Role {
+        SECURE_API_USER, ADMINISTRATOR, SALE_USER, CUSTOMER_USER
+    }
+}
