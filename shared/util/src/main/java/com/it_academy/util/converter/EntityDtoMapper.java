244338f24b5a41846bb6584f29bd58f0d18307a3
PavelDrozd <87943574+PavelDrozd@users.noreply.github.com>
2023-03-23 11:39:50 +0300

    Create EntityDtoMapper for mapping DTO and entity files

---
+++ shared/util/src/main/java/com/it_academy/util/converter/EntityDtoMapper.java
@@ -0,0 +1,23 @@
+package com.it_academy.util.converter;
+
+
+import com.it_academy.data.dto.UserDto;
+import com.it_academy.store.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EntityDtoMapper {
+    private final ModelMapper mapper;
+
+    public UserDto mapToUserDto(User user) {
+        return mapper.map(user, UserDto.class);
+    }
+
+    public User mapToUser(UserDto userDto) {
+        return mapper.map(userDto, User.class);
+    }
+
+}
