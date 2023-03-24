32a35cf94c5aad98117d7c13dee0d4f421ddc29d
PavelDrozd <87943574+PavelDrozd@users.noreply.github.com>
2023-03-24 15:14:52 +0300

    Create ErrorDto and ValidationResultDto classes for processing errors in client module

---
+++ data/src/main/java/com/it_academy/data/dto/web/ErrorDto.java
@@ -0,0 +1,15 @@
+package com.it_academy.data.dto.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDto {
+
+    private String message;
+    private String error;
+
+}
